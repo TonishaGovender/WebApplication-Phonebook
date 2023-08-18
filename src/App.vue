a99bc68ffffa4001f05926efa8672cf30c05624c
Tonisha <nishagovender0205@gmail.com>
2023-08-18 16:00:11 +0200

    Add Validation for phoneNumber

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
           <button @click="isDropdownOpen = !isDropdownOpen">
           <span>+</span>
         </button>
+
         <div v-if="isDropdownOpen" class="dropdown-content">
           <button @click="activeTab = 'add-entry'">Contact</button>
           <button @click="activeTab = 'add-phonebook'">Phonebook</button>
@@ -18,25 +19,48 @@
 
       <div class="tab-content">
         <div class="card-container">
+
           <div class="card" v-if="activeTab === 'search'">
             <h2>Search Contact</h2>
             <!-- Search Form -->
-            <form>
-              <label for="searchName">Name:</label>
-              <input v-model="searchName" id="searchName" type="text" @input="search">
+            <form @submit.prevent="search">
+              <label for="phonebookSelect">Select Phonebook:</label>
+              <select v-model="selectedPhonebook" id="phonebookSelect" required>
+                <option v-for="phonebook in phonebooks" :key="phonebook.id" :value="phonebook.id">{{ phonebook.phonebookName }}</option>
+              </select>
+              <label for="searchName">Name</label>
+              <input v-model="searchName" id="searchName" type="text">
+              <button type="submit">Search</button>
             </form>
+          </div>
+
+          <div v-if="selectedPhonebook">
+            <h2>{{ selectedPhonebook.name }} Details</h2>
+            <ul>
+              <li v-for="entry in selectedPhonebook.entries" :key="entry.id">
+                {{ entry.name }} - {{ entry.number }}
+              </li>
+            </ul>
           </div>
 
           <div class="card" v-else-if="activeTab === 'add-entry'">
             <h2>Add Contact</h2>
             <!-- Form to Add User Entry -->
             <form @submit.prevent="addEntry">
+              <label for="phonebookSelect">Select Phonebook:</label>
+              <select v-model="selectedPhonebook" id="phonebookSelect" required>
+                <option v-for="phonebook in phonebooks" :key="phonebook.id" :value="phonebook.id">{{ phonebook.phonebookName }}</option>
+              </select>
               <label for="name">Name:</label>
               <input v-model="name" id="name" type="text" required>
               <label for="phoneNumber">Phone Number:</label>
-              <input v-model="phoneNumber" id="phoneNumber" type="tel" required>
+              <input v-model="phoneNumber" id="phoneNumber" type="tel" 
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" 
+              maxlength="13" 
+              placeholder="123-456-7890" required>
               <button type="submit">Add Contact</button>
             </form>
+            <div v-if="showMessage" class="message">Contact added successfully!</div>
           </div>
 
           <div class="card" v-else-if="activeTab === 'add-phonebook'">
@@ -55,15 +79,21 @@
 </template>
 
 <script>
+import "./main.js";
 import axios from 'axios';
+
 export default {
   data() {
     return {
       activeTab: 'search',
       isDropdownOpen: false,
+      showMessage: false,
       name: '',
       phoneNumber: '',
       phonebookName: '',
+      entries: [],
+      phonebooks: [],
+      selectedPhonebook: null,
     };
   },
   methods: {
@@ -76,6 +106,11 @@ export default {
       try {
         const response = await axios.post('http://localhost:3000/entries', newEntry);
         console.log('Entry added:', response.data);
+        this.showMessage = true; 
+        setTimeout(() => {
+          this.showMessage = false; 
+          window.location.reload(); 
+        }, 6000); 
       } catch (error) {
         console.error('Error adding entry:', error);
       }
@@ -93,7 +128,52 @@ export default {
         console.error('Error adding phonebook:', error);
       }
     },
+
+    async search() {
+      if (this.selectedPhonebook) {
+        try {
+          const responseEntries = await axios.get('http://localhost:3000/entries');
+          const filteredEntries = responseEntries.data.filter(entry => {
+            const entryName = entry.name.toLowerCase();
+            const phonebook = this.phonebooks.find(pb => pb.id === this.selectedPhonebook);
+            const phonebookName = phonebook ? phonebook.phonebookName.toLowerCase() : '';
+
+            const nameMatches = entryName.includes(this.searchName.toLowerCase());
+            const phonebookMatches = phonebookName.includes(this.searchName.toLowerCase());
+
+            return nameMatches || phonebookMatches;
+          });
+
+          // Update the entries with filtered results
+          this.entries = filteredEntries;
+
+          // Clear the selected phonebook and search name after search
+          this.selectedPhonebook = null;
+          this.searchName = '';
+        } catch (error) {
+          console.error('Error searching:', error);
+        }
+      }
+    },
+    async fetchData() {
+      try {
+        const responseEntries = await axios.get('http://localhost:3000/entries');
+        this.entries = responseEntries.data;
+        const responsePhonebooks = await axios.get('http://localhost:3000/phonebooks');
+        this.phonebooks = responsePhonebooks.data;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    },
   },
+  watch: {
+    selectedPhonebook: 'fetchData', // Watch for changes in selectedPhonebook and trigger fetchData
+  },
+  created() {
+  if (this.activeTab === 'search') {
+    this.fetchData();
+  }
+},
 };
 </script>
 
@@ -155,7 +235,6 @@ export default {
     color: #3498db;
   }
 
-  /* Styling for the dropdown content */
   .dropdown-content {
     position: absolute;
     top: 100%;
