8fdf1a9a8ae5c98d48634e897c6cbab7159798d2
Tonisha <nishagovender0205@gmail.com>
2023-08-20 01:47:26 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
-<script src="./main.js"></script>
-<style src="./styles.css"></style>
+
 
 <template>
   <div class="container">
     <div class="tabs">
       <div class="tab-dropdown">
+        <!-- Add Tab -->
         <button class="tablinks">Add</button>
         <div class="dropdown-content">
           <button @click="openTab('AddEntries')">Add Contact</button>
           <button @click="openTab('AddPhonebook')">Add Phonebook</button>
         </div>
+        <!-- Search Tab -->
         <button class="tablinks" @click="openTab('Phonebook')">Search</button>
       </div>
     </div>
@@ -20,16 +21,19 @@
       <input class="search-input" type="text" v-model="phonebookFilter" placeholder="Enter Phonebook" />
       <ul class="phonebook-list">
         <p>Phonebooks:</p><br>
+        <!-- Display filtered phonebooks -->
         <li v-for="phonebook in filteredPhonebooks" :key="phonebook.id" @click="selectPhonebook(phonebook)">
           {{ phonebook.name }}
         </li>
       </ul>
+      <!-- Display selected phonebook's details -->
       <div v-if="selectedPhonebook" class="phonebook-details">
         <form id="PhonebookDetails">
           <h2>Contact Name</h2>
           <input class="search-input" type="text" v-model="entryFilter" placeholder="Enter Contact Name" />
           <ul class="entry-list">
             <p>Contacts:</p><br>
+            <!-- Display filtered entries -->
             <li v-for="entry in filteredEntries" :key="entry.id">
               {{ entry.name }} - {{ entry.number }}
             </li>
@@ -43,21 +47,32 @@
       <h2>Add Contact</h2>
       <br>
       <p>Select a phonebook</p>
+      <!-- Select phonebook from the list -->
       <select class="select-input" id="phonebookSelect" v-model="selectedPhonebook" required>
         <option v-for="phonebook in phonebooks" :key="phonebook.id" :value="phonebook.id" placeholder="Name">{{ phonebook.name }}</option>
       </select>
+      <!-- Input fields for contact details -->
       <input class="text-input" type="text" v-model="nameInput" placeholder="Name" required>
       <input class="text-input" type="tel" v-model="numberInput" placeholder="098-765-4321"
             pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" maxlength="12" 
-            required />
+            require />
+      <!-- Save button -->
       <button class="btn btn-primary" @click="saveEntry">Save</button>
     </form>
 
     <!-- Add Phonebook Tab -->
     <form id="AddPhonebook" class="tabcontent" @submit.prevent="addPhonebook">
       <h2>Add Phonebook</h2>
+      <!-- Input field for new phonebook name -->
       <input class="text-input" type="text" v-model="newPhonebookInput" placeholder="Phonebook Name" />
+      <!-- Save button -->
       <button type="submit" class="btn btn-primary">Save</button>
     </form>
   </div>
 </template>
+
+<!-- Include JavaScript file -->
+<script src="./main.js"></script>
+
+<!-- Include CSS file -->
+<style src="./styles.css"></style>
